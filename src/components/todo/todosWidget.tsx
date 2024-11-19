0fc340717f5bbb5320d5610445d034fd9090f0ef
Christian Wallervand <cwallervand@gmail.com>
2024-11-19 12:15:26 +0100

    Added button for delete marked todos

--- src/components/todo/todosWidget.tsx
+++ src/components/todo/todosWidget.tsx
@@ -1,6 +1,6 @@
 import { Button } from "~/components/ui/button";
 import { TodoList } from "~/components/todo/todoList";
-import { marksAsCompleted } from "~/server/serverFunctions";
+import { marksAsCompleted, deleteTodos } from "~/server/serverFunctions";
 
 export const TodosWidget = () => {
   return (
@@ -8,7 +8,9 @@ export const TodosWidget = () => {
       <form>
         <div className="selected-todos-actions mb-4 flex flex-row justify-center gap-2">
           <Button formAction={marksAsCompleted}>Mark as completed</Button>
-          {/* <Button variant="destructive">Delete</Button> */}
+          <Button variant="destructive" formAction={deleteTodos}>
+            Delete
+          </Button>
         </div>
         <TodoList />
       </form>
