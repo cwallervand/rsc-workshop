ea914cb4271641ea9295fc2cb1044b439972d329
Christian Wallervand <cwallervand@gmail.com>
2024-11-29 15:40:57 +0100

    Styling "no todos" case

--- src/components/todo/todosWidget.tsx
+++ src/components/todo/todosWidget.tsx
@@ -8,11 +8,9 @@ export const TodosWidget = async () => {
   console.log("### TodosWidget ###");
   const todos = await getTodos();
 
-  console.log("todos", todos);
-
   if (todos.length === 0) {
     return (
-      <Card className="border-green-700 bg-green-700 p-4 text-center text-white">
+      <Card className="border-2 border-green-700 p-4 text-center text-green-700">
         <CardTitle className="flex items-center justify-center gap-2">
           You got nothing to do <CheckBadge className="inline-block size-7" />
         </CardTitle>
