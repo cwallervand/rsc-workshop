032ced48007ac718869d134def79b351ff18faa3
Christian Wallervand <cwallervand@gmail.com>
2025-01-15 10:03:32 +0100

    TodoWidget er klient komponent

--- src/components/todoList/todosWidget.tsx
+++ src/components/todoList/todosWidget.tsx
@@ -1,21 +1,57 @@
-import { getTodos } from "~/server/serverFunctions";
+'use client';
+import { type Todo } from "@prisma/client";
+import { useEffect, useState } from "react";
 
 import { CheckBadge } from "~/components/icons/check-badge";
 import { Card, CardTitle } from "~/components/ui/card";
 import { TodoList } from "~/components/todoList/todoList";
 
-export const TodosWidget = async () => {
-  const todos = await getTodos();
+const delay = (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-  if (todos.length === 0) {
-    return (
-      <Card className="border-2 border-green-700 p-4 text-center text-green-700">
-        <CardTitle className="flex items-center justify-center gap-2">
-          You got nothing to do <CheckBadge className="inline-block size-7" />
-        </CardTitle>
-      </Card>
-    );
+const getTodos = async (): Promise<Todo[]> => {
+  await delay(1000);
+  return Promise.resolve([
+    {
+      id: 1,
+      title: "Oppgave 1: Hello Server Component!",
+      description:
+        "Utforsk hvordan ClientComponent og ServerComponent komponentene blir rendret. Utforsk de forskjellige komposisjonsmønsterene. Hva skjer på server og hva skjer på klienten? ",
+      done: true,
+      createdAt: new Date(),
+    },
+  ]);
+};
+
+
+export const TodosWidget = () => {
+  console.log('### TodosWidget ###');
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getTodos()
+      setTodos(data);
+
+    }
+    void fetchData();
+  }, []);
+
+  if (todos != null) {
+    if (todos.length === 0) {
+      return (
+        <Card className="border-2 border-green-700 p-4 text-center text-green-700">
+          <CardTitle className="flex items-center justify-center gap-2">
+            You got nothing to do <CheckBadge className="inline-block size-7" />
+          </CardTitle>
+        </Card>
+      );
+    }
+    else {
+      return <TodoList todos={todos} />
+    }
+  } else {
+    return null;
   }
-
-  return <TodoList todos={todos}></TodoList>;
 };
