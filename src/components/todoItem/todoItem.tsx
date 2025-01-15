93abf7eb28bfa50cd36b27e76ccb2bd410473852
Christian Wallervand <cwallervand@gmail.com>
2025-01-15 10:03:13 +0100

    Viser kun liste av TODOS i page

--- src/components/todoItem/todoItem.tsx
+++ src/components/todoItem/todoItem.tsx
@@ -1,11 +1,7 @@
 import { type FC } from "react";
 import { type Todo } from "@prisma/client";
 
-import { Card, CardDescription, CardHeader } from "~/components/ui/card";
-
-import { TodoItemMenu } from "~/components/todoItem/todoItemMenu";
-import { ToggleTodoStatus } from "~/components/todoItem/toggleTodoStatus";
-import { TodoTitle } from "~/components/todoItem/todoTitle";
+import { Card, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
 
 type TodoItemProps = {
   todo: Todo;
@@ -14,17 +10,10 @@ type TodoItemProps = {
 export const TodoItem: FC<TodoItemProps> = ({ todo }) => {
   return (
     <Card className="flex flex-row justify-between">
-      <CardHeader className="p-4">
-        <div className="flex flex-row items-center gap-2">
-          <TodoTitle todo={todo} />
-        </div>
-
+      <CardHeader className="p-4 gap-2">
+        <CardTitle>{todo.title}</CardTitle>
         <CardDescription>{todo.description}</CardDescription>
       </CardHeader>
-      <div className="flex flex-col items-center px-4">
-        <TodoItemMenu todo={todo} />
-        <ToggleTodoStatus todo={todo} />
-      </div>
     </Card>
   );
 };
