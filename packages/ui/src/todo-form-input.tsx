c854834a9086a273392acb4cc2d5afab63892439
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2025-01-17 15:09:50 +0100

    Add return type

--- packages/ui/src/todo-form-input.tsx
+++ packages/ui/src/todo-form-input.tsx
@@ -4,7 +4,7 @@ export type TodoInputProps = {
   isPending?: boolean;
 };
 
-export const TodoInput = ({ isPending }: TodoInputProps) => {
+export function TodoInput({ isPending }: TodoInputProps): JSX.Element {
   return (
     <div className="has-[:focus]:shadow-2xl ease-in-out duration-300 shadow-xl transition-all flex rounded-full outline outline-transparent has-[:focus]:outline-orange-600 has-[button:hover]:outline-orange-500 has-[button:focus]:outline-orange-500">
       <div className="flex items-center h-20 w-full rounded-l-full bg-background px-3 py-2 text-base">
