b831b9b48c5e36eb19b4ce43f6315fbab8734285
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2025-01-24 15:03:09 +0100

    Use correct colors

--- packages/ui/src/todo-form-submit.tsx
+++ packages/ui/src/todo-form-submit.tsx
@@ -6,7 +6,7 @@ export interface TodoFormSubmitButtonProps {
 
 export function TodoFormSubmitButton({ isDisabled }: TodoFormSubmitButtonProps): JSX.Element {
   return (
-    <button className="h-20 rounded-none rounded-r-full transition-all duration-300 ease-in-out bg-kantega-red text-lg text-white uppercase font-light px-16 hover:bg-kantega-red-light focus:bg-kantega-red-light active:bg-kantega-red-light outline-none" disabled={isDisabled} type="submit">
+    <button className="h-20 rounded-none rounded-r-full transition-all duration-300 ease-in-out ui-bg-kantega-red text-lg text-white uppercase font-light px-16 hover:ui-bg-kantega-red-light focus:ui-bg-kantega-red-light active:ui-bg-kantega-red-light outline-none" disabled={isDisabled} type="submit">
       Add
     </button>
   );
