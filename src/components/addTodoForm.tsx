f879cf13092e7b8b75b935ff0e3179b99a5fbb7d
Christian Wallervand <cwallervand@gmail.com>
2025-02-06 16:22:13 +0100

    Forbedring

--- src/components/addTodoForm.tsx
+++ src/components/addTodoForm.tsx
@@ -6,7 +6,7 @@ type AddTodoFormProps = {
 export const AddTodoForm = ({ className }: AddTodoFormProps) => {
   return (
     <form className={className}>
-      <p>Implementer skjemaet</p>
+      <p>Implementer skjemaet i denne komponenten</p>
     </form>
   );
 };
