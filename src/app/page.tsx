fec050ab50fa01f00223f8e85fd0fea7f235300d
Christian Wallervand <cwallervand@gmail.com>
2025-02-14 08:37:10 +0100

    Et bedre startpunkt for oppgave 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,18 @@ export default function HomePage() {
   return (
     <main>
       <section>
+        <div className="flex flex-row justify-center gap-4 mb-4">
+          <div className="server">Components rendered on the server are orange</div>
+          <div className="client">Components rendered on the client are blue</div>
+        </div>
+
         <ServerComponent />
 
         {/* <ServerComponent message="Hello there!">
           <ServerComponent message="I am a ServerComponent and I have a ServerComponent parent!" />
         </ServerComponent> */}
 
-        {/* <ClientComponent /> */}
+        <ClientComponent />
 
         {/* <ClientComponent message="I have a ClientComponent child!">
           <ClientComponent message="I am a ClientComponent and I have a ClientComponent parent!" />
