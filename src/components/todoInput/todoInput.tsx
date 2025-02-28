88203f6478f00ba98954ac4d2eb88c8b27ecd5f1
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2025-02-28 14:53:34 +0100

    Tweak more design

--- src/components/todoInput/todoInput.tsx
+++ src/components/todoInput/todoInput.tsx
@@ -2,15 +2,22 @@ import { AddTodoSubmitButton } from "~/components/addTodoSubmitButton";
 
 export const TodoInput = () => {
   return (
-    <div className="has-[:focus]:shadow-2xl ease-in-out duration-300 shadow-xl transition-all flex rounded-full outline outline-transparent has-[:focus]:outline-orange-600 has-[button:hover]:outline-orange-500 has-[button:focus]:outline-orange-500">
-      <div className="flex items-center h-20 w-full rounded-l-full bg-background px-3 py-2 text-base">
-        <div className="flex flex-col w-full">
-          <input className="focus:outline-none px-8 text-lg" type="text" name="title" placeholder="Todo" />
-          <input className="focus:outline-none px-8 text-sm" type="text" name="description" placeholder="Description" />
+    <>
+
+      <div className="ease-in-out duration-300 transition-all flex rounded-full relative">
+        <div className="flex items-center h-20 w-full rounded-l-full px-3 py-2 text-base z-30">
+          <div className="flex flex-col w-full">
+            <input className="focus:outline-none px-8 text-lg bg-transparent" type="text" name="title" placeholder="Todo" />
+            <input className="focus:outline-none px-8 text-sm bg-transparent" type="text" name="description" placeholder="Description" />
+          </div>
         </div>
+
+        <AddTodoSubmitButton />
+
+        <div className="absolute top-[-4px] right-[-4px] bottom-[-4px] left-[-4px] bg-kantega-white rounded-full p-2 z-20"></div>
+        <div className="absolute top-[-8px] right-[-8px] bottom-[-8px] left-[-8px] rounded-full p-1 z-10 bg-gradient-to-t from-[50%] to-[50%] from-kantega-purple to-kantega-transparent"></div>
       </div>
 
-      <AddTodoSubmitButton />
-    </div>
+    </>
   );
 };
