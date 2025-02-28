02ea56e092322bb44d6ded85e109c2e48ba2bded
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2025-02-28 13:51:14 +0100

    Add some Kantega flair

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import { TodosWidget } from "~/components/todoList/todosWidget";
 export default function HomePage() {
   return (
     <>
-      <header className="bg-gradient-to-r from-teal-300 to-indigo-600 text-white py-20">
-        <h1 className="text-9xl font-bold font-bolder text-center m-0">Tudlu</h1>
+      <header className="bg-gradient-to-r from-kantega-purple to-kantega-purple-light text-kantega-white py-20 relative overflow-hidden">
+        <h1 className="text-9xl font-bold font-bolder text-center m-0 relative z-10">Tudlu</h1>
+        <span className="bg-[url(/flyt-dark.svg)] absolute inset-0 bg-no-repeat bg-right-bottom mt-[20px] mb-[-40px] mr-[-65px]" />
       </header>
 
       <main className="container mx-auto px-4">
