60679546c14f8d2114ca8fcfebe9dbe95ce9f7f2
Christian Wallervand <cwallervand@gmail.com>
2025-08-08 10:05:50 +0200

    Change message for component

--- src/components/clientComponentWithServerComponentImport.tsx
+++ src/components/clientComponentWithServerComponentImport.tsx
@@ -6,7 +6,7 @@ import { ServerComponent } from "~/components/serverComponent";
 export const ClientComponentWithServerComponentImport = () => {
   console.log("### ClientComponentWithServerComponentImport ###");
   return (
-    <ClientComponent message="I am a ClientComponent and I have a ServerComponent child (passed as a prop)! Please refresh the page!">
+    <ClientComponent message="I am a ClientComponent and I have a ServerComponent child! Please refresh the page!">
       <ServerComponent message="Rule of thumb: Do not import server components in client components." />
     </ClientComponent>
   );
