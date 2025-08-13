ae8d30d25cca62e5b5449877ddcd0645621f45de
Christian Wallervand <cwallervand@gmail.com>
2025-08-13 14:50:22 +0200

    Bruker form med action istedet for onClick for toggling av status:

--- src/components/todoItem/toggleTodoStatus.tsx
+++ src/components/todoItem/toggleTodoStatus.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { type FC, useState } from "react";
+import { type FC, useOptimistic } from "react";
 import { type Todo } from "@prisma/client";
 
 import { Button } from "~/components/ui/button";
@@ -13,28 +13,34 @@ type ToggleTodoStatusProps = {
 };
 
 export const ToggleTodoStatus: FC<ToggleTodoStatusProps> = ({ todo }) => {
-  const [todoStatus, setTodoStatus] = useState(todo.done);
-  const handleToggleTodoStatus = async () => {
-    const newStatus = !todoStatus;
-    setTodoStatus(newStatus);
+  const [optimisticTodoStatus, setOptimisticTodoStatus] = useOptimistic(todo.done);
+
+  async function updateTodoStatusAction(formData: FormData) {
+    const currentTodoStatus = formData.get("done") === "true";
+    const newStatus = !currentTodoStatus;
+    setOptimisticTodoStatus(newStatus);
     try {
       await updateTodoStatus(todo.id, newStatus);
     } catch {
-      setTodoStatus(todoStatus);
+      console.log("Failed to update todo status");
     }
   };
 
+  console.log("ToggleTodoStatus", optimisticTodoStatus
+  );
   return (
-    <Button
-      variant="ghost"
-      type="submit"
-      size="icon"
-      className="rounded-full relative border-0 [&_svg]:size-8"
-      onClick={handleToggleTodoStatus}
-    >
-      <span className={`border-2 border-current hover:text-kantega-teal-light hover:bg-kantega-white rounded-full w-full h-full relative flex items-center justify-center ${todoStatus ? "text-kantega-teal" : "text-[#e6e6e6]"}`}>
-        <CheckBadge done={todoStatus} className="absolute -left-1 -bottom-2 !w-14 !h-14 -rotate-6" />
-      </span>
-    </Button>
+    <form action={updateTodoStatusAction}>
+      <input type="hidden" name="done" value={optimisticTodoStatus ? "true" : "false"} />
+      <Button
+        variant="ghost"
+        type="submit"
+        size="icon"
+        className="rounded-full relative border-0 [&_svg]:size-8"
+      >
+        <span className={`border-2 border-current hover:text-kantega-teal-light hover:bg-kantega-white rounded-full w-full h-full relative flex items-center justify-center ${optimisticTodoStatus ? "text-kantega-teal" : "text-[#e6e6e6]"}`}>
+          <CheckBadge done={optimisticTodoStatus} className="absolute -left-1 -bottom-2 !w-14 !h-14 -rotate-6" />
+        </span>
+      </Button>
+    </form>
   );
 };
