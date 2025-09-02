75414f640deee2fe5d5d3ad9ed864361583be49f
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2025-09-02 11:17:24 +0200

    Imports and applies custom fonts
    Uses `next/font/google` to import and apply "Noto Sans" and "Gochi Hand" fonts.
    
    Removes the old Google Fonts import from the global CSS file and instead
    uses CSS variables for font families. This improves performance by
    optimizing font loading and allows for easier customization.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,19 @@
-import "~/styles/globals.css";
 import { type Metadata } from "next";
+import { Noto_Sans, Gochi_Hand } from "next/font/google";
+import "~/styles/globals.css";
+
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+  variable: "--font-noto-sans",
+});
+
+const gochiHand = Gochi_Hand({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-gochi-hand",
+});
 
 export const metadata: Metadata = {
   title: "Tudlu",
@@ -11,7 +25,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${notoSans.variable} ${gochiHand.variable}`}>
       <body className="bg-kantega-white">{children}</body>
     </html>
   );
